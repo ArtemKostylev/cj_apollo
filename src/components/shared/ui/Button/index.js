429451c17abddb71f7b719671910ee832f2895e4
ArtemKostylev <brukset29@gmail.com>
2021-11-13 16:51:51 +0300

    fixed button

--- src/components/shared/ui/Button/index.js
+++ src/components/shared/ui/Button/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./button.css";
 
-export const Button = ({ text, onClick }) => {
+export const Button = ({ children, onClick }) => {
   return (
     <button className="button_primary" onClick={onClick}>
-      {text}
+      {children}
     </button>
   );
 };
