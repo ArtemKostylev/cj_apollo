d849ea5381ef5328b81d1e9677ea8e243b46f5ad
Artem Kostylev <a.kostylev@hh.ru>
2025-09-23 09:01:02 +0300

    added build path

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
     base: '/',
+    build: {
+        outDir: '/var/www/akostylev/html'
+    },
     resolve: {
         alias: {
             '~': path.resolve('src')
